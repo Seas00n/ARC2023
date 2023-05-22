75a7107428278ec349a4411b3e5806b6b28cbd2d
Seas00n <60185289+Seas00n@users.noreply.github.com>
2023-05-23 05:11:13 +0800

    1
    1

---
+++ Project/simulation.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+
+from station import *
+from simple import *
+from basic_controller import *
+robot_description_path = "./mini_cheetah/mini_cheetah_simple_v2.urdf"
+drake_path = getDrakePath()
+robot_description_file = "drake/" + os.path.relpath(
+    robot_description_path, start=drake_path
+)
+robot_urdf = FindResourceOrThrow(robot_description_file)
+station = Quad_Station()
+station.AddGround()
+station.AddDog(robot_urdf)
+station.Finalize()
+station.RegisterDogFrame()
+station.ConnectToMeshcatVisualizer()
+planner = station.builder.AddSystem(
+    BasicTrunkPlanner(station.trunk_frame_ids))# type:BasicTrunkPlanner
+controller = station.builder.AddSystem(
+    BasicController(station.plant,5e-3,use_lcm=False))#type:BasicController
+
+station.builder.Connect(
+    station.scene_graph.get_query_output_port(),
+    station.plant.get_geometry_query_input_port())
+
+station.builder.Connect(
+    station.plant.get_geometry_poses_output_port(),
+    station.scene_graph.get_source_pose_port(station.plant.get_source_id())
+)
+station.builder.Connect(
+    planner.GetOutputPort("trunk_geometry"),
+    station.scene_graph.get_source_pose_port(station.trunk_source)
+)
+station.builder.Connect(
+    controller.GetOutputPort("quad_torques"),
+    station.plant.get_actuation_input_port(station.quad)
+)
+station.builder.Connect(station.plant.get_state_output_port(),
+                        controller.GetInputPort("quad_state"))
+logger = LogVectorOutput(controller.GetOutputPort("output_metrics"),builder=station.builder)
+
+diagram = station.builder.Build()#type:Diagram
+diagram.set_name("diagram")
+diagram_context = diagram.CreateDefaultContext()
+# plt.figure()
+# plot_system_graphviz(diagram, max_depth=2)
+# plt.show()
+
+
+simulator = Simulator(diagram,diagram_context)
+simulator.set_target_realtime_rate(1.0)
+plant_context = diagram.GetMutableSubsystemContext(station.plant, diagram_context)
+q0 = np.asarray([ 1.0, 0.0, 0.0, 0.0,     # base orientation
+                  0.0, 0.0, 0.3,          # base position
+                  0.0,-0.8, 1.6,
+                  0.0,-0.8, 1.6,
+                  0.0,-0.8, 1.6,
+                  0.0,-0.8, 1.6])
+qd0 = np.zeros(station.plant.num_velocities())
+station.plant.SetPositions(plant_context,q0)
+station.plant.SetVelocities(plant_context,qd0)
+simulator.AdvanceTo(50)
---
+++ Project/station.py
@@ -0,0 +1,76 @@
+from pydrake.all import (getDrakePath, DiagramBuilder, SceneGraph, FindResourceOrThrow,
+                         MultibodyPlant, Parser, RigidTransform, CoulombFriction, Box,
+                        GeometryFrame, GeometryInstance, MakePhongIllustrationProperties, Sphere,
+                        LogVectorOutput,StartMeshcat, MeshcatVisualizer, MeshcatVisualizerParams,plot_system_graphviz,
+                        Simulator, Diagram)
+
+import numpy as np
+
+class Quad_Station(Diagram):
+    scene_graph: SceneGraph
+    plant: MultibodyPlant
+
+    def __init__(self):
+        Diagram.__init__(self)
+        self.set_name("Quad Station")
+        self.builder = DiagramBuilder()
+        self.scene_graph = self.builder.AddSystem(SceneGraph())
+        self.scene_graph.set_name("scene_graph")
+        self.plant = self.builder.AddSystem(MultibodyPlant(time_step=5e-3))
+        self.plant.RegisterAsSourceForSceneGraph(self.scene_graph)
+        self.plant.set_name("plant")
+        self.parser = Parser(self.plant)
+
+    def AddDog(self, model_urdf):
+        self.quad = self.parser.AddModelFromFile(model_urdf, 'quad')
+    def AddGround(self):
+        surface_friction = CoulombFriction(
+            static_friction=1.0,
+            dynamic_friction=1.0
+        )
+        self.plant.RegisterCollisionGeometry(
+            self.plant.world_body(),
+            RigidTransform(),
+            Box(50, 50, 0.001),
+            "ground_collision",
+            surface_friction
+        )
+        self.plant.RegisterVisualGeometry(
+            self.plant.world_body(),
+            RigidTransform(),
+            Box(50, 50, 0.001),
+            "ground_visual",
+            np.array([0.5, 0.5, 0.5, 0.0])
+        )
+
+    def Finalize(self):
+        self.plant.Finalize()
+
+    def RegisterDogFrame(self):
+        self.trunk_source = self.scene_graph.RegisterSource("trunk")
+        self.trunk_frame = GeometryFrame("trunk")
+        self.scene_graph.RegisterFrame(self.trunk_source, self.trunk_frame)  # 在躯干创建坐标系
+        trunk_shape = Box(0.4, 0.2, 0.1)
+        trunk_color = np.array([0.1, 0.1, 0.1, 0.4])
+        X_trunk = RigidTransform()
+        trunk_geometry = GeometryInstance(X_trunk, trunk_shape, "trunk")
+        trunk_geometry.set_illustration_properties(MakePhongIllustrationProperties(trunk_color))
+        self.scene_graph.RegisterGeometry(self.trunk_source, self.trunk_frame.id(), trunk_geometry)
+        self.trunk_frame_ids = {"trunk": self.trunk_frame.id()}
+        for foot in ["lf", "rf", "lh", "rh"]:
+            foot_frame = GeometryFrame(foot)
+            self.scene_graph.RegisterFrame(self.trunk_source, foot_frame)
+            foot_shape = Sphere(0.02)
+            X_foot = RigidTransform()
+            foot_geometry = GeometryInstance(X_foot, foot_shape, foot)
+            foot_geometry.set_illustration_properties(MakePhongIllustrationProperties(trunk_color))
+            self.scene_graph.RegisterGeometry(self.trunk_source, foot_frame.id(), foot_geometry)
+            self.trunk_frame_ids[foot] = foot_frame.id()
+    def ConnectToMeshcatVisualizer(self):
+        self.meshcat = StartMeshcat()
+        self.visualizer = MeshcatVisualizer.AddToBuilder(
+            self.builder,
+            self.scene_graph,
+            self.meshcat,
+            MeshcatVisualizerParams()
+        )
